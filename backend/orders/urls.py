5f20f4c25ae02f3f3498eee08b223239bcf9e38c
Wangyu369 <joshuaganas28@gmail.com>
2025-05-18 07:03:49 +0800

    Update urls.py

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet, CartViewSet
 
 router = DefaultRouter()
-# router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'cart', CartViewSet, basename='cart')
 router.register(r'', OrderViewSet, basename='order')
 
 
